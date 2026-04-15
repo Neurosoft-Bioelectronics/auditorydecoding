04024ed8fb6136c37f295a541ea341b1967a6e57
estefanysuarez <laura.suarez@mail.mcgill.ca>
2026-04-15 15:53:40 -0400

    Updated skip_sessions list for Monkeys pipeline

--- pipelines/neurosoft_monkeys_2026/pipeline.py
+++ pipelines/neurosoft_monkeys_2026/pipeline.py
@@ -14,14 +14,6 @@ from auditorydecoding import NeurosoftPipeline
 
 class Pipeline(NeurosoftPipeline):
     brainset_id = "neurosoft_monkeys_2026"
-    
+
     # unannotated sessions
-    skip_sessions = [
-        "sub-03_ses-02_task-AcousStim_acq-RH_desc-raw",
-        "sub-03_ses-03_task-AcousStim_acq-RH_desc-raw",
-        "sub-03_ses-04_task-AcousStim_acq-RH_desc-raw",
-        "sub-05_ses-03_task-AcousStim_acq-LH_desc-raw",
-        "sub-05_ses-03_task-AcousStim_acq-RH_desc-raw",
-        "sub-06_ses-01_task-AcousStim_acq-LH_desc-raw",
-        "sub-06_ses-01_task-AcousStim_acq-RH_desc-raw",
-    ]
+    skip_sessions = []
