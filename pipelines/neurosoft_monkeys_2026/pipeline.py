071f389b518e9d7229d560094ba16326cf46290c
Laura Suarez <31476198+estefanysuarez@users.noreply.github.com>
2026-04-20 14:25:48 -0400

    Added pipeline to process Monkey data (#9)
    Added pipeline for monkey data and turned auditory_decoding as a package
    
    ---------
    
    Co-authored-by: Milo Sobral <milosobral@gmail.com>

---
+++ pipelines/neurosoft_monkeys_2026/pipeline.py
@@ -0,0 +1,19 @@
+# /// brainset-pipeline
+# python-version = "3.11"
+# dependencies = [
+#   "mne==1.11.0",
+#   "mne-bids==0.18",
+#   "scikit-learn==1.7.2",
+#   "brainsets",
+#   "auditorydecoding@git+https://github.com/Neurosoft-Bioelectronics/auditorydecoding@main",
+# ]
+# ///
+
+from auditorydecoding import NeurosoftPipeline
+
+
+class Pipeline(NeurosoftPipeline):
+    brainset_id = "neurosoft_monkeys_2026"
+
+    # unannotated sessions
+    skip_sessions = []
